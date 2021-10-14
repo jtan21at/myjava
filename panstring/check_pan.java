cd91f0eb1be935551939a355193f42f808cd4804
anoymity <jtan21@go.pasadena.edu>
2021-10-14 14:38:06 -0700

    minor changes

--- panstring/check_pan.java
+++ panstring/check_pan.java
@@ -11,17 +11,23 @@ public class check_pan {
 	public static void main(String[] args) {
 		String str_in, reverse = "";
 	      Scanner input = new Scanner(System.in);// TODO Auto-generated method stub
+	      System.out.println("how many times you like to try?" );
+	      Scanner times=new Scanner(System.in);
+	      Integer time=Integer.valueOf(times.nextLine());
+	      
+	    	while (time>0)	{  
 	      System.out.println("Enter a string:");
 	      str_in = input.nextLine();
 	      for ( int i = str_in.length() - 1; i >= 0; i-- )
-	          reverse = reverse + str_in.charAt(i);
-	  
-	       if (reverse.equals(str_in))
-	          System.out.println(str_in+" is a palindrome");
-	       else
-	          System.out.println(str_in+" is not a palindrome");
-	      
-	      
+	    	  reverse = reverse + str_in.charAt(i);
+
+	      if (reverse.equals(str_in))
+	    	  System.out.println(str_in+" is a palindrome");
+	      else
+	    	  System.out.println(str_in+" is not a palindrome");
+
+	      time--;
+	    	}
 	}
 
 }
